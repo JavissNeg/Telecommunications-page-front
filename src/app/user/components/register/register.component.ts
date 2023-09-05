1cbf05dced667c33be23c4df2b928ec61097a0a3
Javiss Negrete <negretbtino@hotmail.com>
2023-09-05 15:35:33 -0600

    Add comprobation password

--- src/app/user/components/register/register.component.ts
+++ src/app/user/components/register/register.component.ts
@@ -1,10 +1,111 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+
+export class RegisterComponent implements OnInit{
+
+  show: boolean = true;
+  registerForm!: FormGroup;
+
+  constructor( public readonly fb: FormBuilder, public router:Router ) { }
+  
+  ngOnInit(): void {
+    this.registerForm = this.initForm();
+  }
+
+
+
+  initForm(): FormGroup {
+    function validatePasswordMatch(control: AbstractControl): { [key: string]: boolean } | null {
+      const password = control.parent?.get('password')?.value;
+      const password_confirmation = control?.value;
+      
+      if (password === password_confirmation) {
+        return null;
+      } else {
+        return { passwordMatch: true };
+      }
+    }
+
+    return this.fb.group({
+      name: [
+        'Javier', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]
+      ],
+      lastname: [
+        'Negrete', [Validators.required, Validators.minLength(5), Validators.maxLength(25)]
+      ],
+      mail: [
+        'negretbtino@hotmail.com', [Validators.required, Validators.email, Validators.maxLength(30)]
+      ],
+      phone: [
+        '7297056530', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]
+      ],
+      username: [
+        'Javier', [Validators.required, Validators.minLength(4), Validators.maxLength(15)]
+      ],
+      password: [
+        '12345678', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]
+      ],
+      password_confirmation: [
+        '12345678', [Validators.required, Validators.minLength(8), Validators.maxLength(20), validatePasswordMatch]
+      ],
+    });
+  }
+  
+  save(): void {
+    console.log('save');
+    
+  }
+
+  checkError( idError: string, controlInput: string ): boolean {
+    const htmlElement = document.getElementById(idError);
+    const htmlControlInput = this.registerForm.controls[controlInput]
+
+    if (htmlElement) {
+
+      if ( htmlControlInput.hasError('required') ) {
+        htmlElement.textContent = 'Este campo es obligatorio';
+        return true;
+
+      } else {
+        if ( htmlControlInput.hasError('minlength') ) {
+          const minLength = htmlControlInput.errors?.['minlength']?.requiredLength;
+          htmlElement.textContent = `Este campo debe tener al menos ${minLength} caracteres`;
+          return true;
+
+        } else {
+          if ( htmlControlInput.hasError('maxlength') ) {
+            const maxLength = htmlControlInput.errors?.['maxlength']?.requiredLength;
+            htmlElement.textContent = `Este campo debe tener menos de ${maxLength} caracteres`;
+            return true;
+            
+          } else {
+            if (htmlControlInput.hasError('email')) {
+              htmlElement.textContent = 'El correo electrónico no es válido';
+              return true;
+            } else {
+              if (htmlControlInput.hasError('passwordMatch')) {
+                htmlElement.textContent = 'Las contraseñas ingresadas no coinciden';
+                return true;
+              } else { return false; }
+            }
+          }
+        }
+      }
+
+    }
+    
+    return false;
+  }
+
+  goLink(link: string): void {
+    this.router.navigateByUrl(link);
+  }
 
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +13,8 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
